03b03aa24fb316de1e0f06ba9f011d12f14e96f8
georgid <georgi.dzhambazov@upf.edu>
2016-09-01 21:50:49 +0200

    -added src.contour_selection_VD.contours_to_vocal: At a given frame if there is at least one melodic contour, it is considered as voiced. It replaces gm.melody_from_clf with viterbi decoding - added  src.melodyExtractionFromSalienceFunction.overlap_contours: serialization of contour overlaps

---
+++ src/contour_classification/Parameters.py
@@ -0,0 +1,10 @@
+'''
+Created on Aug 31, 2016
+
+@author: georgid
+'''
+
+class Parameters(object):
+    datasetIKala  = True
+    iKala_annotation_URI = "/home/georgid/Documents/iKala/PitchLabel/"
+    useTimbre = True
